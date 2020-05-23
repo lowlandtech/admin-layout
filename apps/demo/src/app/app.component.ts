bc20d52dd61a25d58bb50a4675863121a5b2a9ab
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-05-23 16:32:54 +0200

    chore: change npm scope and angular prefix

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'admin-layout-root',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
