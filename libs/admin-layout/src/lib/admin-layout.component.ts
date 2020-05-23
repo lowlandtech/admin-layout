bbaffdfc4019ebb1a1b09b25c11d6a106686da5c
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-05-23 16:48:50 +0200

    chore: add admin-layout-component
    $ ng generate @schematics/angular:component --name=admin-layout --project=admin-layout --style=scss --export --flat --inlineTemplate --no-interactive

---
+++ libs/admin-layout/src/lib/admin-layout.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-admin-layout',
+  template: `
+    <p>
+      admin-layout works!
+    </p>
+  `,
+  styleUrls: ['./admin-layout.component.scss']
+})
+export class AdminLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
