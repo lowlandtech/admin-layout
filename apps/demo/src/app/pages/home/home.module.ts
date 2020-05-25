ed409d8ac8bdd98ba400e4edd0f976ee6553281f
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-05-25 13:52:22 +0200

    chore: add home page component

--- apps/demo/src/app/pages/home/home.module.ts
+++ apps/demo/src/app/pages/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
+import { HomeRoutingModule } from './home.routes';
+import { HomeComponent } from './home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
